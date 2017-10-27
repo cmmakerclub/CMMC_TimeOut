220e649bb38dfdf116396a2a86484e052c309455
tan <nat@cmmc.io>
2017-10-27 16:34:33 +0700

    Add yield method.

--- src/CMMC_TimeOut.h
+++ src/CMMC_TimeOut.h
@@ -10,10 +10,12 @@ class CMMC_TimeOut
       CMMC_TimeOut() { }
       ~CMMC_TimeOut() {} 
       bool is_timeout(); 
+      void yield(); 
       void timeout_ms(uint32_t t); 
     private:
       uint32_t _current_tick;
       uint32_t _next_tick;
+      uint32_t _user_timeout_ms;
 };
 
 #endif //CMMC_TimeOut_H
